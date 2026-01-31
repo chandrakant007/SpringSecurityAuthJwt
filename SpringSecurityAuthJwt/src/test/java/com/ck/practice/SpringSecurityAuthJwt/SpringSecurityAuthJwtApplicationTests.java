d389ec1ec4ba447d650fbc8e7a05eb120262ba8e
Chandrakant <34542932+chandrakant007@users.noreply.github.com>
2026-01-31 17:23:22 +0530

    Add files via upload

---
+++ SpringSecurityAuthJwt/src/test/java/com/ck/practice/SpringSecurityAuthJwt/SpringSecurityAuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ck.practice.SpringSecurityAuthJwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
